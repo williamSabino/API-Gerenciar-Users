e09f45eb50521894146675a544e5a75270bc7e79
Willi_sabino <williamsabino64@gmail.com>
2024-04-02 18:29:29 -0300

    initial cmmit

---
+++ GerenciaUsers/GerenciaUsers/src/main/java/br/com/will/GerenciaUsers/GerenciaUsersApplication.java
@@ -0,0 +1,13 @@
+package br.com.will.GerenciaUsers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciaUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciaUsersApplication.class, args);
+	}
+
+}
